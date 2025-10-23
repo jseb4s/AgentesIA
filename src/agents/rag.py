4cb706f8a5d954ae1a43275d31dfa269997eb903
jseb4s <smoncada485@gmail.com>
2025-10-23 15:19:33 -0500

    add chaining and structured output notebooks and update rag.py

--- src/agents/rag.py
+++ src/agents/rag.py
@@ -17,26 +17,62 @@ llm = llm.bind_tools([file_search_tool])
 
 class State(MessagesState):
     customer_name: str
-    my_age: int
+    phone: str
+    my_age: str
 
-def node_1(state: State):
+
+# RAG con Chaining de nodos, es decir, pasos secuenciales. Cada paso usualmente usa un LLM.
+# Este caso se usa mucho en RRSS para generar tweets e imagenes a partir de tweets.
+
+from pydantic import BaseModel, Field
+class ContactInfo(BaseModel):
+    #Esta clase representa la información de contacto de una persona, y el LLM la llenará automáticamente a partir de un texto dado.
+    #Sera a su vez la base para el extractor
+    """A person's contact information."""
+    name: str = Field(description="The name of the person")
+    email: str = Field(description="The email address of the person")
+    phone: str = Field(description="The phone number of the person")
+    age: str = Field(description="The age of the person")
+
+
+llm_with_structured_output = init_chat_model(model = "gemini-2.5-flash", 
+                      model_provider="google_genai",
+                      temperature=0
+                      )
+llm_with_structured_output = llm_with_structured_output.with_structured_output(ContactInfo)
+
+
+# El nodo extractor no es conversacional, sino que extrae información del usuario a partir de su mensaje.
+# Evalua la información que hay en el historial de la conversación y extrae la información que falta.
+def extractor(state: State): # Nodo extractor, que extrae información del usuario si no está presente.
+    history = state["messages"]
+    customer_name = state.get("customer_name", None)
     new_state: State = {}
-    if state.get("customer_name") is None:
-        new_state["customer_name"] = "John Doe"
-    else:
-        new_state["my_age"] = random.randint(18, 99)
+    if customer_name is None or len(history) >= 10:
+        schema = llm_with_structured_output.invoke(history)
+        new_state["customer_name"] = schema.name
+        new_state["phone"] = schema.phone
+        new_state["my_age"] = schema.age
+    return new_state
 
+def conversation(state: State): # Nodo de conversación, que usa el LLM para responder mensajes del usuario.
+    new_state: State = {}
     history = state["messages"]
     last_message = history[-1]
-    ai_message = llm.invoke(last_message.text)
+
+    customer_name = state.get("customer_name", "John Doe")
+    system_message = f"You are a helpful assistant that can answer questions about the customer {customer_name}."
+    ai_message = llm.invoke([("system", system_message), ("user", last_message.text)])
     new_state["messages"] = [ai_message]
     return new_state
 
 from langgraph.graph import StateGraph, START, END
 
 builder = StateGraph(State)
-builder.add_node("node_1", node_1)
-builder.add_edge(START, "node_1")
-builder.add_edge("node_1", END)
+builder.add_node("conversation", conversation)
+builder.add_node("extractor", extractor)
+builder.add_edge(START, "extractor")
+builder.add_edge("extractor", "conversation")
+builder.add_edge("conversation", END)
 
 agent = builder.compile()
